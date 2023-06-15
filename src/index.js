7274810eed5c8e95f0e47bbaaf144fdffbe842d2
AmithN68 <amithn68@gmail.com>
2023-06-15 11:45:21 +0530

    com

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./style.scss";
+import "./Sass/style.scss";
 import App from "./routers/app";
 
 createRoot(document.getElementById("root")).render(<App />);
